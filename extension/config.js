21f13756fb9f1605ebc53123fcea73cd6b06110c
nobodykr <nobodykr@users.noreply.github.com>
2026-08-23 21:29:10 +0100

    release: pin stable extension channel

--- extension/config.js
+++ extension/config.js
@@ -3,6 +3,7 @@ globalThis.__FISCALIDADE_CONFIG__ = Object.freeze({
   "publicOrigin": "https://fiscalida.de",
   "apiBase": "https://fiscalida.de/api/v1",
   "extension": true,
+  "channel": "stable",
   "remoteCodeAllowed": false,
   "offersUrl": chrome.runtime.getURL("offers.json")
 });
